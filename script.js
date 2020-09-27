5ad927af601f6be8770300cd1de76d9ae50bfad8
satya kavya <satyakavya24@gmail.com>
2020-09-27 10:29:32 -0500

    add timer

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 var scoreEl = document.getElementById("highscore");
 var timerEl = document.getElementById("timer");
 var containerEl= document.getElementById("container");
-var headingEl=document.getElementById("heading";)
+var headingEl=document.querySelector(".heading");
+var introductionEl=document.querySelector(".introduction");
+var buttonEl=document.querySelector("#startbtn");
+console.log(buttonEl);
+console.log("hello");
+var mainEl=document.getElementById("main");
+
+buttonEl.addEventListener("click",function(){
+    console.log("hello");
+        headingEl.textContent="";
+        introductionEl.textContent="";
+        var time=60;
+     var timeInterval=setInterval(function(){
+              time--;
+              timerEl.textContent=time;
+    
+            if(time===0)
+            {
+                headingEl.textContent="game over";
+            }
+    
+        },1000);
+    
+});
+
+
+
 
