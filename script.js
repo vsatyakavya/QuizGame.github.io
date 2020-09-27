cc1c43fac561b4573e934d660a62b45eb1cd6c62
satya kavya <satyakavya24@gmail.com>
2020-09-27 09:43:16 -0500

    initial commit, add static html page

---
+++ script.js
@@ -0,0 +1,5 @@
+var scoreEl = document.getElementById("highscore");
+var timerEl = document.getElementById("timer");
+var containerEl= document.getElementById("container");
+var headingEl=document.getElementById("heading";)
+
