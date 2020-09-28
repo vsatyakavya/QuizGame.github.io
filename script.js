2f80a4dad14d4096bd029ced8c43f0f1718655f3
satya kavya <satyakavya24@gmail.com>
2020-09-28 17:37:31 -0500

    replay option, highscore display and delete option

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var hignScoreEl = document.getElementById("highscore");
 var timerEl = document.getElementById("timer");
+
+
 var containerEl = document.getElementById("container");
+
 var firstPageEl = document.querySelector("#firstPage");
 // var headingEl = document.querySelector(".heading");
 // var introductionEl = document.querySelector(".introduction");
 var buttonEl = document.querySelector("#startbtn");
+
 var resultDiv = document.getElementById("allDone");
 var scoreDiv = document.getElementById("score");
 var initialEl = document.getElementById("initial");
 var submitButton = document.getElementById("submitButton");
+
 var finalPageEl=document.getElementById("finalPage");
 var highScoreInfoEl=document.querySelector(".highScoreInfo");
+var goBackEl=document.querySelector("#goback");
+var clearHighScoreEl=document.querySelector("#clearHighScore");
 
 
 
@@ -29,25 +36,35 @@ var h4El = document.createElement("h4");
 
 
 
-btn1.setAttribute("style", "display:flex;padding:5px;margin:5px;background-color:coral");
-btn2.setAttribute("style", "display:flex;padding:5px;margin:5px;background-color:coral")
-btn3.setAttribute("style", "display:flex;padding:5px;margin:5px;background-color:coral")
-btn4.setAttribute("style", "display:flex;padding:5px;margin:5px;background-color:coral")
+btn1.setAttribute("class","optionbuttons");
+btn2.setAttribute("class","optionbuttons");
+
+btn3.setAttribute("class","optionbuttons");
+btn4.setAttribute("class","optionbuttons");
+
 mainEl.setAttribute("style", "text-align:left");
+optionsEl.setAttribute("style","text-align:left");
+pEl.setAttribute("style","text-align:left");
 
-
-
-var time = 30;
-var i = 0;
+var score = 0;
+var quiz;
+var time;
+var i;
 var timeInterval;
 var initial;
 
 buttonEl.addEventListener("click", function () {
+    time=30;
+    i=0;
     // headingEl.textContent = "";
     // introductionEl.textContent = "";
     // buttonEl.style.visibility = "hidden";
   
      firstPageEl.setAttribute("style","display:none;");
+     mainEl.setAttribute("style","display:block");
+     optionsEl.setAttribute("style","display:block");
+
+
     
      timeInterval = setInterval(function () {
         time--;
@@ -55,7 +72,7 @@ buttonEl.addEventListener("click", function () {
 
         if (time === 0) {
             clearInterval(timeInterval);
-
+            showResult();
 
         }
 
@@ -68,10 +85,11 @@ buttonEl.addEventListener("click", function () {
     optionsEl.appendChild(btn3);
     optionsEl.appendChild(btn4);
     containerEl.appendChild(mainEl);
+    console.log(mainEl,pEl);
     containerEl.appendChild(h4El);
 
 
-    var quiz = [{
+     quiz = [{
         "question": "commonly used data types DO NOT include:",
         "answers": {
             "a": "strings",
@@ -107,99 +125,74 @@ buttonEl.addEventListener("click", function () {
 
     looping();
 
-    function looping() {
-        if (i < quiz.length) {
-            pEl.textContent = quiz[i].question;
-            btn1.textContent = quiz[i].answers.a;
-            btn2.textContent = quiz[i].answers.b;
-            btn3.textContent = quiz[i].answers.c;
-            btn4.textContent = quiz[i].answers.d;
+    
 
 
-        }
-        else if (time == 0 || i == quiz.length) {
+     score = 0;
+    // btn1.addEventListener("click", function () {
 
-            showResult();
-            scoreDiv.textContent = score;
-            resultDiv.setAttribute("style", "display:block,max-width:500px;margin:0 auto;width:50%;text-align:left");
-            clearInterval(timeInterval);
+    //     if (btn1.textContent === quiz[i].correctAnswer) {
+    //         score++;
+    //         h4El.textContent = "correct";
 
-        }
-    }
+    //     }
+    //     else {
+    //         h4El.textContent = "wrong";
+    //         time -= 10;
+    //     }
 
-    function showResult() {
-        mainEl.setAttribute("style","display:none");
-        optionsEl.setAttribute("style","display:none");
-
-        h4El.textContent = "";
-
-
-    }
+    //     i++;
+    //     looping();
 
 
 
-    var score = 0;
-    btn1.addEventListener("click", function () {
+    // });
+    // btn2.addEventListener("click", function () {
+    //     if (btn2.textContent === quiz[i].correctAnswer) {
+    //         score++;
+    //         h4El.textContent = "correct";
+    //     }
+    //     else {
+    //         h4El.textContent = "wrong";
+    //         time -= 10;
+    //     }
+    //     i++;
 
-        if (btn1.textContent === quiz[i].correctAnswer) {
-            score++;
-            h4El.textContent = "correct";
-
-        }
-        else {
-            h4El.textContent = "wrong";
-            time -= 10;
-        }
-
-        i++;
-        looping();
-
+    //     looping();
+    // });
 
 
-    });
-    btn2.addEventListener("click", function () {
-        if (btn2.textContent === quiz[i].correctAnswer) {
-            score++;
-            h4El.textContent = "correct";
-        }
-        else {
-            h4El.textContent = "wrong";
-            time -= 10;
-        }
-        i++;
+    // btn3.addEventListener("click", function () {
+    //     if (btn3.textContent === quiz[i].correctAnswer) {
+    //         score++;
+    //         h4El.textContent = "correct";
+    //     }
+    //     else {
+    //         h4El.textContent = "wrong";
+    //         time -= 10;
 
-        looping();
-    });
+    //     }
+    //     i++;
+    //     looping();
+    // });
+   
 
-
-    btn3.addEventListener("click", function () {
-        if (btn3.textContent === quiz[i].correctAnswer) {
-            score++;
-            h4El.textContent = "correct";
-        }
-        else {
-            h4El.textContent = "wrong";
-            time -= 10;
-
-        }
-        i++;
-        looping();
-    });
-    btn4.addEventListener("click", function () {
-        if (btn4.textContent === quiz[i].correctAnswer) {
-            score++;
-            h4El.textContent = "correct";
-        }
-        else {
-            h4El.textContent = "wrong";
-            time -= 10;
-        }
-        i++;
+        
+    // {
+    //     if (btn4.textContent === quiz[i].correctAnswer) {
+    //         score++;
+    //         h4El.textContent = "correct";
+    //     }
+    //     else {
+    //         h4El.textContent = "wrong";
+    //         time -= 10;
+    //     }
+    //     i++;
 
 
-        looping();
-    });
-
+    //     looping();
+    // });
+   
     submitButton.addEventListener("click", function () {
         initial = document.getElementById('initial').value;
         localStorage.setItem(initial, score);
@@ -227,11 +220,74 @@ buttonEl.addEventListener("click", function () {
 
         console.log('highest score user', highScoreUser);
     });
+   
 
+    goBackEl.addEventListener("click",function( ){
+        
+        firstPageEl.setAttribute("style","display:block");
+        finalPageEl.setAttribute("style","display:none");
+        
+    })
+    
+    clearHighScoreEl.addEventListener("click",function(){
+        highScoreInfoEl.textContent="";
+        highScoreInfoEl.setAttribute("style","display:none")
+        console.log("hellooooooooo");
+    });
 
 
 });
  
+// var goBackEl=document.querySelector("#goback");
+// var clearHighScoreEl=document.querySelector("#clearHignScore");
 
 
+btn1.addEventListener("click", checkAnswer )
+btn2.addEventListener("click", checkAnswer )
+btn3.addEventListener("click", checkAnswer )
+btn4.addEventListener("click", checkAnswer )
+function checkAnswer(event){
+    if (event.target.textContent === quiz[i].correctAnswer) {
+        score++;
+        h4El.textContent = "correct";
 
+    }
+    else {
+        h4El.textContent = "wrong";
+        time -= 10;
+    }
+    i++;
+
+    setTimeout(function(){
+        h4El.textContent="";
+    },500)
+    looping()
+}
+function looping() {
+    if (i < quiz.length) {
+        pEl.textContent = quiz[i].question;
+        btn1.textContent = quiz[i].answers.a;
+        btn2.textContent = quiz[i].answers.b;
+        btn3.textContent = quiz[i].answers.c;
+        btn4.textContent = quiz[i].answers.d;
+
+
+    }
+    else if (time == 0 || i == quiz.length) {
+        console.log("helloooooooooooooo")
+        showResult();
+        clearInterval(timeInterval);
+
+    }
+}
+
+function showResult() {
+    mainEl.setAttribute("style","display:none");
+    optionsEl.setAttribute("style","display:none");
+    scoreDiv.textContent = score;
+    resultDiv.setAttribute("style", "display:block,max-width:500px;margin:0 auto;width:50%;text-align:left");
+
+    h4El.textContent = "";
+
+
+}
