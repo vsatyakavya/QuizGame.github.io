4d9bcf3ccf99945a6f499ec033b842d9c43e31b2
satya kavya <satyakavya24@gmail.com>
2020-09-29 16:53:58 -0500

    final code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# QuizGame.github.io
+
+# Application Does:
+* Quiz Game
+
+# Technologies used:
+* HTML, CSS, JavaScript
+
+# Features:
+
+* Shows question when click on start button
+* Timer
+* After answer question presents with another question
+* Incorrect answer reduces the timer value by 10 seconds
+* when Timer reaches 0 or all questions are answered game ends
+* Display user score
+* Can save user details
+* Can see High SCore
+* Replay option
+
+
+# Challenges:
+* Giving Variable names
+* Showing results
+
+
+# Sample web page
+![picture](quiz.png)
+
+# Github Page 
+https://vsatyakavya.github.io/PasswordGenerator.github.io/
+
+
+
+# Github Repository
+https://github.com/vsatyakavya/PasswordGenerator.github.io
+
+@copyright 2020
--- script.js
+++ script.js
@@ -1,12 +1,11 @@
+var viewHighScore = document.getElementById("viewHighScore");
+
 var hignScoreEl = document.getElementById("highscore");
 var timerEl = document.getElementById("timer");
-
 
 var containerEl = document.getElementById("container");
 
 var firstPageEl = document.querySelector("#firstPage");
-// var headingEl = document.querySelector(".heading");
-// var introductionEl = document.querySelector(".introduction");
 var buttonEl = document.querySelector("#startbtn");
 
 var resultDiv = document.getElementById("allDone");
@@ -14,14 +13,14 @@ var scoreDiv = document.getElementById("score");
 var initialEl = document.getElementById("initial");
 var submitButton = document.getElementById("submitButton");
 
-var finalPageEl=document.getElementById("finalPage");
-var highScoreInfoEl=document.querySelector(".highScoreInfo");
-var goBackEl=document.querySelector("#goback");
-var clearHighScoreEl=document.querySelector("#clearHighScore");
+var finalPageEl = document.getElementById("finalPage");
+var highScoreInfoEl = document.querySelector(".highScoreInfo");
+var goBackEl = document.querySelector("#goback");
+var clearHighScoreEl = document.querySelector("#clearHighScore");
 
 
 
-
+//creating dynamic div's and buttons
 var mainEl = document.createElement("div");
 var pEl = document.createElement("p");
 
@@ -32,19 +31,16 @@ var btn3 = document.createElement("button");
 var btn4 = document.createElement("button");
 var h4El = document.createElement("h4");
 
-//
 
+btn1.setAttribute("class", "optionbuttons");
+btn2.setAttribute("class", "optionbuttons");
+btn3.setAttribute("class", "optionbuttons");
+btn4.setAttribute("class", "optionbuttons");
 
-
-btn1.setAttribute("class","optionbuttons");
-btn2.setAttribute("class","optionbuttons");
-
-btn3.setAttribute("class","optionbuttons");
-btn4.setAttribute("class","optionbuttons");
 
 mainEl.setAttribute("style", "text-align:left");
-optionsEl.setAttribute("style","text-align:left");
-pEl.setAttribute("style","text-align:left");
+optionsEl.setAttribute("style", "text-align:left");
+pEl.setAttribute("style", "text-align:left;font-size:30px;");
 
 var score = 0;
 var quiz;
@@ -52,44 +48,36 @@ var time;
 var i;
 var timeInterval;
 var initial;
+var highScoreUser = "";
+var highestScore = 0;
 
 buttonEl.addEventListener("click", function () {
-    time=30;
-    i=0;
-    // headingEl.textContent = "";
-    // introductionEl.textContent = "";
-    // buttonEl.style.visibility = "hidden";
-  
-     firstPageEl.setAttribute("style","display:none;");
-     mainEl.setAttribute("style","display:block");
-     optionsEl.setAttribute("style","display:block");
+    time = 60;
+    i = 0;
+    firstPageEl.setAttribute("style", "display:none;");
+    mainEl.setAttribute("style", "display:block");
+    optionsEl.setAttribute("style", "display:block");
 
-
-    
-     timeInterval = setInterval(function () {
+    timeInterval = setInterval(function () {
         time--;
         timerEl.textContent = time;
-
         if (time === 0) {
             clearInterval(timeInterval);
             showResult();
-
         }
 
     }, 1000);
 
     mainEl.appendChild(pEl);
-    mainEl.appendChild(optionsEl)
     optionsEl.appendChild(btn1);
     optionsEl.appendChild(btn2);
     optionsEl.appendChild(btn3);
     optionsEl.appendChild(btn4);
+    mainEl.appendChild(optionsEl)
     containerEl.appendChild(mainEl);
-    console.log(mainEl,pEl);
     containerEl.appendChild(h4El);
-
-
-     quiz = [{
+ //questions
+    quiz = [{
         "question": "commonly used data types DO NOT include:",
         "answers": {
             "a": "strings",
@@ -97,7 +85,7 @@ buttonEl.addEventListener("click", function () {
             "c": "alerts",
             "d": "numbers"
         },
-        "correctAnswer": "strings"
+        "correctAnswer": "alerts"
     },
     {
         "question": "The condition in an if/else statement is enclosed within_________:",
@@ -110,7 +98,7 @@ buttonEl.addEventListener("click", function () {
         "correctAnswer": "paranthesis"
     },
     {
-        "question": "Arrays in javaScript can be used to store",
+        "question": "Arrays in javaScript can be used to store _________",
         "answers": {
             "a": "numbers and strings",
             "b": "other arrays",
@@ -118,138 +106,42 @@ buttonEl.addEventListener("click", function () {
             "d": "all of the above"
         },
         "correctAnswer": "all of the above"
+    },
+    {
+        "question": "String values must ne enclosed within _______ When being assigned to variables",
+        "answers": {
+            "a": "commas",
+            "b": "curly brackets",
+            "c": "quotes",
+            "d": "paranthesis"
+        },
+        "correctAnswer": "quotes"
+    },
+    {
+        "question": "A very usefil tol used during development and debugging for printing content to the debugger is:",
+        "answers": {
+            "a": "javaScript",
+            "b": "terminal/bash",
+            "c": "for loops",
+            "d": "console.log"
+        },
+        "correctAnswer": "console.log"
     }
+    
 
     ];
 
-
     looping();
-
-    
-
-
-     score = 0;
-    // btn1.addEventListener("click", function () {
-
-    //     if (btn1.textContent === quiz[i].correctAnswer) {
-    //         score++;
-    //         h4El.textContent = "correct";
-
-    //     }
-    //     else {
-    //         h4El.textContent = "wrong";
-    //         time -= 10;
-    //     }
-
-    //     i++;
-    //     looping();
-
-
-
-    // });
-    // btn2.addEventListener("click", function () {
-    //     if (btn2.textContent === quiz[i].correctAnswer) {
-    //         score++;
-    //         h4El.textContent = "correct";
-    //     }
-    //     else {
-    //         h4El.textContent = "wrong";
-    //         time -= 10;
-    //     }
-    //     i++;
-
-    //     looping();
-    // });
-
-
-    // btn3.addEventListener("click", function () {
-    //     if (btn3.textContent === quiz[i].correctAnswer) {
-    //         score++;
-    //         h4El.textContent = "correct";
-    //     }
-    //     else {
-    //         h4El.textContent = "wrong";
-    //         time -= 10;
-
-    //     }
-    //     i++;
-    //     looping();
-    // });
-   
-
-        
-    // {
-    //     if (btn4.textContent === quiz[i].correctAnswer) {
-    //         score++;
-    //         h4El.textContent = "correct";
-    //     }
-    //     else {
-    //         h4El.textContent = "wrong";
-    //         time -= 10;
-    //     }
-    //     i++;
-
-
-    //     looping();
-    // });
-   
-    submitButton.addEventListener("click", function () {
-        initial = document.getElementById('initial').value;
-        localStorage.setItem(initial, score);
-        console.log(initial);
-         var highScoreUser="";
-        var highestScore = 0;
-        var storedValues = Object.entries(localStorage);
-        for (var i in storedValues) {
-            var curr = storedValues[i];
-            if (curr[1] > highestScore) {
-                highestScore = curr[1];
-                highScoreUser = curr[0];
-                console.log(highestScore);
-                console.log(highScoreUser);
-            }
-
-        }
-        document.getElementById('initial').value = '';
-        resultDiv.setAttribute("style","display:none")
-        finalPageEl.setAttribute("style","display:block;color:blue;");
-        highScoreInfoEl.textContent=highScoreUser+" "+highestScore;
-        highScoreInfoEl.setAttribute("style","background-color:skyblue;");
-       
-       // highScoreInfoEl.textContent= highScoreUser;
-       // highScoreInfoEl.setAttribute("style","background-color:#cccccc;")
-
-        console.log('highest score user', highScoreUser);
-    });
-   
-
-    goBackEl.addEventListener("click",function( ){
-        
-        firstPageEl.setAttribute("style","display:block");
-        finalPageEl.setAttribute("style","display:none");
-        
-    })
-    
-    clearHighScoreEl.addEventListener("click",function(){
-        localStorage.clear();
-        highScoreInfoEl.textContent="";
-        highScoreInfoEl.setAttribute("style","display:none")
-        console.log("hellooooooooo");
-       
-    });
-
-
+    score = 0;
 });
- 
-// var goBackEl=document.querySelector("#goback");
-// var clearHighScoreEl=document.querySelector("#clearHignScore");
 
+ //when click on options
 
-btn1.addEventListener("click", checkAnswer )
-btn2.addEventListener("click", checkAnswer )
-btn3.addEventListener("click", checkAnswer )
-btn4.addEventListener("click", checkAnswer )
-function checkAnswer(event){
+btn1.addEventListener("click", checkAnswer)
+btn2.addEventListener("click", checkAnswer)
+btn3.addEventListener("click", checkAnswer)
+btn4.addEventListener("click", checkAnswer)
+function checkAnswer(event) {
     if (event.target.textContent === quiz[i].correctAnswer) {
         score++;
         h4El.textContent = "correct";
@@ -261,9 +153,9 @@ function checkAnswer(event){
     }
     i++;
 
-    setTimeout(function(){
-        h4El.textContent="";
-    },500)
+    setTimeout(function () {
+        h4El.textContent = "";
+    }, 500)
     looping()
 }
 function looping() {
@@ -277,7 +169,7 @@ function looping() {
 
     }
     else if (time == 0 || i == quiz.length) {
-        console.log("helloooooooooooooo")
+        
         showResult();
         clearInterval(timeInterval);
 
@@ -285,8 +177,8 @@ function looping() {
 }
 
 function showResult() {
-    mainEl.setAttribute("style","display:none");
-    optionsEl.setAttribute("style","display:none");
+    mainEl.setAttribute("style", "display:none");
+    optionsEl.setAttribute("style", "display:none");
     scoreDiv.textContent = score;
     resultDiv.setAttribute("style", "display:block,max-width:500px;margin:0 auto;width:50%;text-align:left");
 
@@ -294,3 +186,64 @@ function showResult() {
 
 
 }
+//when submit the answer
+
+submitButton.addEventListener("click", function () {
+    initial = document.getElementById('initial').value;
+    localStorage.setItem(initial, score);
+    console.log(initial);
+    
+    var storedValues = Object.entries(localStorage);
+    for (var i in storedValues) {
+        var curr = storedValues[i];
+        if (curr[1] > highestScore) {
+            highestScore = curr[1];
+            highScoreUser = curr[0];
+            
+        }
+
+    }
+    document.getElementById('initial').value = '';
+    resultDiv.setAttribute("style", "display:none")
+    finalPageEl.setAttribute("style", "display:block;color:blue;");
+    highScoreInfoEl.textContent = highScoreUser + " " + highestScore;
+    highScoreInfoEl.setAttribute("style", "background-color:#ccc;text-align:left;");
+
+
+    console.log('highest score user', highScoreUser);
+});
+
+//click go back button
+
+goBackEl.addEventListener("click", function () {
+    storedValues="";
+    highScoreInfoEl.textContent = "";
+    highScoreInfoEl.setAttribute("style", "display:none")
+    firstPageEl.setAttribute("style", "display:block");
+    finalPageEl.setAttribute("style", "display:none");
+
+})
+//click clear high score
+clearHighScoreEl.addEventListener("click", function () {
+    localStorage.clear();
+    storedValues="";
+    highScoreInfoEl.textContent = "";
+    highScoreInfoEl.setAttribute("style", "display:none")
+    
+
+});
+
+viewHighScore.addEventListener("click",function(){
+    viewHighScore.textContent=highestScore;
+    setTimeout(function () {
+        viewHighScore.textContent = "viewHighScore";
+    }, 1500)
+    looping()
+})
+
+
+//});
+
+
+
+
